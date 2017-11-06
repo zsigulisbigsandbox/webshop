82e3d232b154f9ca4205b743755b5d72f84bd231
zsiguli <markovichzsigmond@gmail.com>
2017-11-06 18:29:26 +0100

    Initialalize our webshop project

---
+++ src/main/java/com/webshop/fancy/FancyApplication.java
@@ -0,0 +1,12 @@
+package com.webshop.fancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancyApplication.class, args);
+	}
+}
